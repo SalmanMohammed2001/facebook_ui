7c434a99729bbf61cd030d28246313d283096c69
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-24 19:49:44 +0530

    facebook main bar implementation

--- lib/screen/facebook_ui.dart
+++ lib/screen/facebook_ui.dart
@@ -10,6 +10,50 @@ class FacebookHome extends StatefulWidget {
 class _FacebookHomeState extends State<FacebookHome> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      body: SafeArea(
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                const Text(
+                  "facebook",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                ),
+                const Spacer(
+                  flex: 1,
+                ),
+                CircleAvatar(
+                  radius: 15,
+                  backgroundColor: Colors.grey.shade800,
+                  child: const Icon(
+                    Icons.search,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ),
+                const SizedBox(
+                  width: 6,
+                ),
+                CircleAvatar(
+                  radius: 15,
+                  backgroundColor: Colors.grey.shade800,
+                  child: const Icon(
+                    Icons.message,
+                    color: Colors.white,
+                    size: 15,
+                  ),
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
