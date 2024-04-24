bc264799063d5d15fdba1673ca93a502af7b753d
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-24 19:18:36 +0530

    facbook home page create

---
+++ lib/screen/facebook_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FacebookHome extends StatefulWidget {
+  const FacebookHome({super.key});
+
+  @override
+  State<FacebookHome> createState() => _FacebookHomeState();
+}
+
+class _FacebookHomeState extends State<FacebookHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
