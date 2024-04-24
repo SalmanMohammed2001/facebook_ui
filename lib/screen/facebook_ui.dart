3dd560a0198c9c2b4c07857a169a87c28e913c25
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-24 22:40:48 +0530

    Appar icon ok

--- lib/screen/facebook_ui.dart
+++ lib/screen/facebook_ui.dart
@@ -9,13 +9,82 @@ class FacebookHome extends StatefulWidget {
 }
 
 class _FacebookHomeState extends State<FacebookHome> {
+  final profilePic =
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsQ-YHX2i3RvTDDmpfnde4qyb2P8up7Wi3Ww&usqp=CAU";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade900,
-      body: const SafeArea(
-        child: Column(children: [MainBar()]),
+      body: SafeArea(
+        child: Column(children: [
+          const MainBar(),
+          const SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const ManIcon(
+                icon: Icons.home,
+              ),
+              const ManIcon(
+                icon: Icons.people,
+              ),
+              const ManIcon(
+                icon: Icons.ondemand_video_outlined,
+              ),
+              const ManIcon(
+                icon: Icons.notifications,
+              ),
+              Container(
+                width: 30,
+                height: 30,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle, color: Colors.blue),
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Stack(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(profilePic),
+                      ),
+                      Positioned(
+                        bottom: 2,
+                        right: 2,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey.shade700,
+                          radius: 5,
+                          child: const Icon(
+                            Icons.menu,
+                            size: 8,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
+        ]),
       ),
+    );
+  }
+}
+
+class ManIcon extends StatelessWidget {
+  const ManIcon({super.key, required this.icon});
+
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      icon,
+      color: Colors.white,
+      size: 30,
     );
   }
 }
