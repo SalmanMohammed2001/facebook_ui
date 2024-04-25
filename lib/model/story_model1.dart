7ec804ecb0e467ec941ec1cac3b9910b70f2cf35
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-25 12:12:33 +0530

    create StoryModel class and data patch

---
+++ lib/model/story_model1.dart
@@ -0,0 +1,13 @@
+class SortyModel {
+  String userName;
+  String userImage;
+  String story;
+  String uid;
+
+  SortyModel({
+    required this.story,
+    required this.uid,
+    required this.userImage,
+    required this.userName,
+  });
+}
--- lib/screen/facebook_ui.dart
+++ lib/screen/facebook_ui.dart
@@ -1,3 +1,4 @@
+import 'package:facebook_ui/model/story_model1.dart';
 import 'package:facebook_ui/widget/icon_bar.dart';
 import 'package:facebook_ui/widget/main_bar.dart';
 import 'package:facebook_ui/widget/new_post_bar.dart';
@@ -13,6 +14,56 @@ class FacebookHome extends StatefulWidget {
 class _FacebookHomeState extends State<FacebookHome> {
   final profilePic =
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsQ-YHX2i3RvTDDmpfnde4qyb2P8up7Wi3Ww&usqp=CAU";
+
+  final List<SortyModel> stories = [
+    SortyModel(
+        story: "story",
+        uid: "uid",
+        userImage: "userImage",
+        userName: "userName"),
+    SortyModel(
+      story:
+          "https://cdn.shopify.com/s/files/1/0344/2586/4328/articles/persons_hand_delivering_a_vibrant_bouquet_from_Tooka_Flo_6616f8fb-b2ff-4071-bc7c-d3e32b7ba9e2_1024x1024.png?v=1686679475",
+      uid: "100",
+      userImage: "https://randomuser.me/api/portraits/women/60.jpg",
+      userName: "Gina Bowman",
+    ),
+    SortyModel(
+      story:
+          "https://images.unsplash.com/photo-1529619768328-e37af76c6fe5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eSUyMG5pZ2h0fGVufDB8fDB8fHww&w=1000&q=80",
+      uid: "200",
+      userImage: "https://randomuser.me/api/portraits/men/33.jpg",
+      userName: "Robert Hudson",
+    ),
+    SortyModel(
+      story:
+          "https://upload.wikimedia.org/wikipedia/commons/4/4c/Beauty_of_Sigiriya_by_Binuka.jpg",
+      uid: "300",
+      userImage: "https://randomuser.me/api/portraits/women/5.jpg",
+      userName: "Georgia Dixon",
+    ),
+    SortyModel(
+      story:
+          "https://img.olympicchannel.com/images/image/private/t_s_w960/t_s_16_9_g_auto/f_auto/primary/peml11q6maltpwsohdmi",
+      uid: "400",
+      userImage: "https://randomuser.me/api/portraits/men/11.jpg",
+      userName: "Jon Reyes",
+    ),
+    SortyModel(
+      story:
+          "https://assets.mspimages.in/wp-content/uploads/2021/09/Samsung-Galaxy-M52-5G-Brand-Story.png",
+      uid: "500",
+      userImage: "https://randomuser.me/api/portraits/men/32.jpg",
+      userName: "Jose Holt",
+    ),
+    SortyModel(
+      story:
+          "https://images.unsplash.com/photo-1580483046931-aaba29b81601?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cnVzc2lhbiUyMGdpcmx8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+      uid: "600",
+      userImage: "https://randomuser.me/api/portraits/women/79.jpg",
+      userName: "Jill Jacobs",
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +94,7 @@ class _FacebookHomeState extends State<FacebookHome> {
             height: 176,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: 6,
+              itemCount: stories.length,
               itemBuilder: (context, index) {
                 return Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -103,7 +154,7 @@ class _FacebookHomeState extends State<FacebookHome> {
                                       borderRadius: BorderRadius.circular(15),
                                       image: DecorationImage(
                                           image: NetworkImage(
-                                            profilePic,
+                                            stories[index].story,
                                           ),
                                           fit: BoxFit.cover,
                                           colorFilter: ColorFilter.mode(
@@ -111,32 +162,37 @@ class _FacebookHomeState extends State<FacebookHome> {
                                             BlendMode.darken,
                                           ))),
                                 ),
-                                Container(
-                                  width: 35,
-                                  height: 35,
-                                  decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      border: Border.all(
-                                        color: Colors.blue,
-                                        width: 1,
-                                      )),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(2.0),
-                                    child: CircleAvatar(
-                                      backgroundImage: NetworkImage(profilePic),
+                                Positioned(
+                                  left: 5,
+                                  top: 5,
+                                  child: Container(
+                                    width: 35,
+                                    height: 35,
+                                    decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.all(
+                                          color: Colors.blue,
+                                          width: 1,
+                                        )),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(2.0),
+                                      child: CircleAvatar(
+                                        backgroundImage: NetworkImage(
+                                            stories[index].userImage),
+                                      ),
                                     ),
                                   ),
                                 ),
-                                const Align(
+                                Align(
                                   alignment: Alignment.bottomCenter,
                                   child: Padding(
-                                    padding: EdgeInsets.only(
+                                    padding: const EdgeInsets.only(
                                       left: 5,
                                       bottom: 5,
                                     ),
                                     child: Text(
-                                      " Ajith Kumar",
-                                      style: TextStyle(
+                                      stories[index].userName,
+                                      style: const TextStyle(
                                           color: Colors.white,
                                           fontWeight: FontWeight.w500),
                                     ),
